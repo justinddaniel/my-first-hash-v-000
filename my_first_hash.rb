05d97ee2425d1705e7a176833996be7efe45ba40
Justin Daniel <justinddaniel@gmail.com>
2017-09-19 22:33:35 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -8,14 +8,11 @@ end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
-  #your code here!
+shipping_manifest["oil paintings"]
 end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
-  #your code here
-  #remember to return the shipping_manifest hash
-
+shipping_manifest["hoo hash"] = 4
+return shipping_manifest
 end
