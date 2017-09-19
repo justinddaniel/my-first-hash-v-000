f52ace876b22bc31a92fc33dd84adc5fafe8baa3
Justin Daniel <justinddaniel@gmail.com>
2017-09-19 22:34:29 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,6 +13,6 @@ end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-shipping_manifest["hoo hash"] = 4
+shipping_manifest["pearl necklace"] = 1
 return shipping_manifest
 end
